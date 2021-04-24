a974ac3b8a491921820d26206c07d7f0f3993a7b
Alpfonso <lukasilov1@gmail.com>
2021-04-24 15:18:40 +0200

    Inital Ankete class & minor GUI window change

--- Source/src/eiar/Ankete.java
+++ Source/src/eiar/Ankete.java
@@ -1,5 +1,21 @@
 package eiar;
 
 public class Ankete extends Alati {
-
+	private String nazivAnkete;
+	private String opisAnkete;
+	private String sugestija;
+	
+	public Ankete(String naziv, String opis ) {
+		this.nazivAnkete = naziv;
+		this.opisAnkete = opis;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void Promjeni_naziv(String novi_naziv) {
+		this.nazivAnkete = novi_naziv;
+	}
+	
+	public void Promjeni_opis(String novi_opis) {
+		this.opisAnkete = novi_opis;
+	}
 }
