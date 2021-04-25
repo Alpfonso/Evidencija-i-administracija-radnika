e1de3e63943050c942ad26622ff06ff7a7827b82
Alpfonso <lukasilov1@gmail.com>
2021-04-25 13:59:06 +0200

    Changed DB data types for zaposlenik; Created first insertion method

--- Source/src/eiar/DB_Connect.java
+++ Source/src/eiar/DB_Connect.java
@@ -1,18 +1,39 @@
 package eiar;
 
 import java.sql.*;
+import java.time.LocalDate;
 
 public class DB_Connect {
-  public static void main( String args[] ) {
-      Connection con = null;
-      
-      try {
-         Class.forName("org.sqlite.JDBC");
-         con = DriverManager.getConnection("jdbc:sqlite:../Database/EIAR_DB.db");
-      } catch ( Exception e ) {
-         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-         System.exit(0);
-      }
-      System.out.println("Opened database successfully");
-   }
+	public DB_Connect() {
+		// TODO Auto-generated constructor stub
+	}
+	public void Create_User(String ime, String prezime, LocalDate datum_rodenja, String grad_stanovanja, String ulica_i_broj, String email, String OIB, 
+			String telefon, String obrazovanje, Time radno_vrijeme_start, Time radno_vrijeme_end, String razina_ovlasti){
+	      	Connection con = null;
+	      	Statement stmt = null;
+	      
+	      	try {
+	      		Class.forName("org.sqlite.JDBC");
+	      		// Establish connection to DB via jdbc driver
+	      		con = DriverManager.getConnection("jdbc:sqlite:../Database/EIAR_DB.db");
+	      		con.setAutoCommit(false);
+	      		System.out.println("Opened database successfully");
+	      		
+	      		// stmt obj used to execute SQL statements
+	      		stmt = con.createStatement();
+	      		String sql = "INSERT INTO zaposlenici('ime', 'prezime', 'datum_rodenja', 'grad_stanovanja', 'ulica_i_broj', 'email',"
+	      				+ " 'OIB', 'telefon', 'obrazovanje', 'razina_ovlasti', 'radno_vrijeme_start', 'radno_vrijeme_end') VALUES "
+	      				+ "('" + ime + "','" + prezime + "','" + datum_rodenja + "','" + grad_stanovanja + "','" + ulica_i_broj + "','" + email
+	      				+ "','" + OIB + "','" + telefon + "','" + obrazovanje + "','" + razina_ovlasti + "','" + radno_vrijeme_start + "','" + 
+	      				radno_vrijeme_end + "')"; 
+	      		stmt.executeUpdate(sql);
+	      		stmt.close();
+	            con.commit();
+	            con.close();
+	      	} catch ( Exception e ) {
+	      		System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      		System.exit(0);
+	      	}
+	      	System.out.println("User record created");
+	}
 }
--- Source/src/eiar/Main.java
+++ Source/src/eiar/Main.java
@@ -1,11 +1,18 @@
 package eiar;
+import java.sql.*;
+import java.time.LocalDate;
 
 public class Main {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		GUI.basic_display();
 		
-
+		DB_Connect db_object = new DB_Connect();
+		LocalDate birth = LocalDate.of(1999, 4, 14);
+		Time start = new Time(8,0,0);
+		Time end = new Time(12,0,0);
+		db_object.Create_User("Dzhon", "Cena", birth , "Labin", "Ulica 9", "nesto@nesto.nesto", "988", "0988", "Neplaceni freelancer", start, end, "Vlasnik");
 	}
 
 }
