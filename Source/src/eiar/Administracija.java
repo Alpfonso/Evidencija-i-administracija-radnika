4030cf77e1582c166e71956bd5d562f7abe022da
Piero Battelli <pierobattelli@gmail.com>
2021-04-25 13:10:57 +0200

    Added variables in class Alati

--- Source/src/eiar/Administracija.java
+++ Source/src/eiar/Administracija.java
@@ -1,5 +1,13 @@
 package eiar;
 
+import java.util.Date;
+
 public class Administracija extends Radnik {
+
+	public Administracija(String ime, String prez, Date dat, String g_s, String u_s, String email, int OIB, String tele,
+			String obrazovanje, String razina_ovlasti, Date r_v_p, Date r_v_k, int s_d) {
+		super(ime, prez, dat, g_s, u_s, email, OIB, tele, obrazovanje, razina_ovlasti, r_v_p, r_v_k, s_d);
+		// TODO Auto-generated constructor stub
+	}
 	
 }
--- Source/src/eiar/Alati.java
+++ Source/src/eiar/Alati.java
@@ -2,4 +2,8 @@ package eiar;
 
 public class Alati {
 
+	String naziv;
+	String opis;
+	String ime_autora;
+	
 }
