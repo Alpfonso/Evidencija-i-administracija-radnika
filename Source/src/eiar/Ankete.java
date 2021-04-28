8688ec8ca3b5ba0c050a6176f7184883cf565ef6
Piero Battelli <pierobattelli@gmail.com>
2021-04-28 14:51:00 +0200

    Modified Ankete and Financijska_izvjesca class

--- Source/src/eiar/Ankete.java
+++ Source/src/eiar/Ankete.java
@@ -1,21 +1,19 @@
 package eiar;
 
 public class Ankete extends Alati {
-	private String nazivAnkete;
-	private String opisAnkete;
+	
 	private String sugestija;
 	
-	public Ankete(String naziv, String opis ) {
-		this.nazivAnkete = naziv;
-		this.opisAnkete = opis;
-		// TODO Auto-generated constructor stub
+	public Ankete(String naziv, String opis, String ime_autora, String sugestija) {
+		super(naziv, opis, ime_autora);
+		this.sugestija = sugestija;
 	}
-	
+
 	public void Promjeni_naziv(String novi_naziv) {
-		this.nazivAnkete = novi_naziv;
+		this.setNaziv(novi_naziv);
 	}
 	
 	public void Promjeni_opis(String novi_opis) {
-		this.opisAnkete = novi_opis;
+		this.setOpis(novi_opis);
 	}
 }
--- Source/src/eiar/Chat.java
+++ Source/src/eiar/Chat.java
@@ -2,4 +2,8 @@ package eiar;
 
 public class Chat extends Alati {
 
+	public Chat(String naziv, String opis, String ime_autora) {
+		super(naziv, opis, ime_autora);
+	}
+
 }
