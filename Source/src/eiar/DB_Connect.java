05df341f4ca66746e00f3fd72997444421cf87df
Alpfonso <lukasilov1@gmail.com>
2021-04-29 15:15:47 +0200

    Reworked Create user into universial Insert method; Added delete and Update methods

--- Source/src/eiar/DB_Connect.java
+++ Source/src/eiar/DB_Connect.java
@@ -15,37 +15,48 @@ public class DB_Connect {
         }
         return conn;
     }
-	public void Create_User(String ime, String prezime, LocalDate datum_rodenja, String grad_stanovanja, String ulica_i_broj, String email, String OIB, 
-			String telefon, String obrazovanje, Time radno_vrijeme_start, Time radno_vrijeme_end, String razina_ovlasti){
-	      	Connection con = null;
-	      	Statement stmt = null;
-	      
-	      	try {
-	      		Class.forName("org.sqlite.JDBC");
-	      		// Establish connection to DB via jdbc driver
-	      		con = DriverManager.getConnection("jdbc:sqlite:../Database/EIAR_DB.db");
-	      		con.setAutoCommit(false);
-	      		System.out.println("Opened database successfully");
-	      		
-	      		// stmt obj used to execute SQL statements
-	      		stmt = con.createStatement();
-	      		String sql = "INSERT INTO zaposlenici('ime', 'prezime', 'datum_rodenja', 'grad_stanovanja', 'ulica_i_broj', 'email',"
-	      				+ " 'OIB', 'telefon', 'obrazovanje', 'razina_ovlasti', 'radno_vrijeme_start', 'radno_vrijeme_end') VALUES "
-	      				+ "('" + ime + "','" + prezime + "','" + datum_rodenja + "','" + grad_stanovanja + "','" + ulica_i_broj + "','" + email
-	      				+ "','" + OIB + "','" + telefon + "','" + obrazovanje + "','" + razina_ovlasti + "','" + radno_vrijeme_start + "','" + 
-	      				radno_vrijeme_end + "')";
-	      		stmt.executeUpdate(sql);
-	      		stmt.close();
-	            con.commit();
-	            con.close();
-	      	} catch ( Exception e ) {
-	      		System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	      		System.exit(0);
-	      	}
-	      	System.out.println("User record created");
+    
+	public void Insert_table_data(String[] atributes, Object[] data, String table_name, int data_id)
+	{
+		StringBuilder str = new StringBuilder();
+		if(atributes.length == data.length) {	// Build INSERTION string
+			str.append("'" + table_name + "' (");
+			for(int i = 0; i < atributes.length;i++) {	// Building column names into a workable sql
+				str.append("'" + atributes[i] + "'");
+				if(i<atributes.length-1) {
+					str.append(", ");
+				}
+			}
+			str.append(") VALUES (");
+			for(int i = 0; i < data.length;i++) {	// Building values into a workable sql
+				str.append("'" + data[i] + "'");
+				if(i<data.length-1) {
+					str.append(", ");
+				}
+			}
+			str.append(")");
+		}
+		else {
+			System.out.println("Nesukladne kolicine podataka");	// In case of array size missmatch print error
+		}
+		
+		String sql = "INSERT INTO " + str.toString();
+		System.out.println(sql);
+		
+		Connection conn = this.connect();
+		Statement stmt = null;
+		
+		try {
+			stmt = conn.createStatement();
+			stmt.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
-	public ResultSet Fetch_Table_Data(String table_name, int data_id){
+	public ResultSet Fetch_table_data(String table_name, int data_id){
 		String sql = "SELECT * FROM '" + table_name + "' WHERE id = '" + data_id + "'";
 		Connection conn = this.connect();
 		Statement stmt;
@@ -59,5 +70,53 @@ public class DB_Connect {
 		}
 	    
 		return rs;
+	}
+	
+	public ResultSet Delete_table_data(String table_name, int data_id){	// Delete entry from table | Send back ResultSet with deleted table info
+		String sql_delete = "DELETE FROM '" + table_name + "' WHERE id = '" + data_id + "'";
+		String sql_select = "SELECT * FROM '" + table_name + "' WHERE id = '" + data_id + "'";
+		Connection conn = this.connect();
+		Statement stmt_sel, stmt_del;	// Create 2 separate statements for deletion & selection
+		ResultSet rs = null;
+		try {
+			stmt_sel = conn.createStatement();
+			stmt_del = conn.createStatement();
+			rs = stmt_sel.executeQuery(sql_select);
+			stmt_del.executeUpdate(sql_delete);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;	// Return ResultSet data for confirmation
+	}
+	
+	public void Update_table_data(String[] atributes, Object[] data, String table_name, int data_id) {	// Update table entry based on atributes and data
+		StringBuilder str = new StringBuilder();
+		if(atributes.length == data.length) {	// Build SET part of sql string
+			for(int i = 0; i < atributes.length;i++) {
+				str.append("'" + atributes[i] + "' = '" + data[i] + "' ");
+				if(i<atributes.length-1) {
+					str.append(", ");
+				}
+			}
+			System.out.println(str.toString());
+		}
+		else {
+			System.out.println("Nesukladne kolicine podataka");	// In case of array missmatch print error
+		}
+		
+		String sql = "UPDATE '" + table_name + "' SET " + str.toString() + "WHERE id ='" + data_id + "'";	
+		
+		Connection conn = this.connect();
+		Statement stmt = null;
+		
+		try {
+			stmt = conn.createStatement();
+			stmt.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
--- Source/src/eiar/Main.java
+++ Source/src/eiar/Main.java
@@ -13,12 +13,37 @@ public class Main {
 		Time start = new Time(8,0,0);
 		Time end = new Time(12,0,0);
 		//db_object.Create_User("Dzhon", "Cena", birth , "Labin", "Ulica 9", "nesto@nesto.nesto", "988", "0988", "Neplaceni freelancer", start, end, "Vlasnik");
-		ResultSet rs = db_object.Fetch_Table_Data("zaposlenici", 7);
+		ResultSet rs = db_object.Fetch_table_data("zaposlenici", 7);
 		
 		while (rs.next()) {
 		     System.out.println(rs.getInt("id") +  "\t" + 
 		                        rs.getString("ime") + " " + rs.getString("prezime") + " " + rs.getString("radno_vrijeme_end"));
 		}
+		
+		String[] str = new String[5];
 
+		str[0] = new String("ime");
+		str[1] = new String("prezime");
+		str[2] = new String("radno_vrijeme_start");
+		str[3] = new String("radno_vrijeme_end");
+		str[4] = new String("OIB");
+
+		
+		Object[] arr = new Object[5];
+
+		arr[0] = new String("Novi");
+		arr[1] = new String("Juzer");
+		arr[2] = new Time(14,45,22);
+		arr[3] = new Time(14,30,59);
+		arr[4] = new String("515380085");
+		
+		//db_object.Update_table_data(str, arr, "zaposlenici", 11);
+		db_object.Insert_table_data(str, arr, "zaposlenici", 11);
+		
+		/*ResultSet rs_del = db_object.Delete_table_data("zaposlenici", 10);
+		while (rs_del.next()) {
+		     System.out.println("Deleted user: " + rs_del.getInt("id") + " " + rs_del.getString("ime") + " " + rs_del.getString("prezime"));
+		}*/
+		
 	}
 }
