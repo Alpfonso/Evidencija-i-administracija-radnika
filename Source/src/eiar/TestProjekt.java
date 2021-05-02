38acb84bc8480143f64e059b6d075a4e2469ea19
vedranmatic <vmatic@riteh.hr>
2021-05-02 11:02:28 +0200

    testprojekt

--- Source/src/eiar/TestProjekt.java
+++ Source/src/eiar/TestProjekt.java
@@ -3,12 +3,23 @@ package eiar;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import java.util.Date;
 
 public class TestProjekt {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Date date = new Date();
+		Date rvp = new Date(); //radno vrijeme poèetak
+		Date rvk = new Date(); //radno vrijeme kraj
+		Nadredeni n1 = new Nadredeni("Luka","Lukic",date,"Rijeka","Rijecka ulica","LukaLukic@gmail.com",12345678,"0980981234","Riteh","Nadreden",rvp,rvk,2);
+		
+		Date datum_pocetak = new Date();
+		Date datum_kraj = new Date();
+
+		Projekt p1 = new Projekt("Marko",n1,datum_pocetak,datum_kraj);
+		
+		
 	}
 
 }
