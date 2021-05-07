bed8bf02821b7a4406480d2ee3845c367aab7d47
Piero Battelli <pierobattelli@gmail.com>
2021-05-07 14:25:33 +0200

    Added method & constructor to class HR

--- Source/src/eiar/HR.java
+++ Source/src/eiar/HR.java
@@ -5,7 +5,7 @@ import java.util.Date;
  * Human resources
  * <br>Conducts surveys
  */
-public class HR extends Odrzavanje {
+public class HR extends Administracija {
 
 	public HR(String ime, String prez, Date dat, String g_s, String u_s, String email, int OIB, String tele,
 			String obrazovanje, String razina_ovlasti, Date r_v_p, Date r_v_k, int s_d) {
@@ -13,4 +13,17 @@ public class HR extends Odrzavanje {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	public Ankete napravi_anketu (String naziv, String opis, String ime_autora, String sugestija) {
+		
+		Ankete anketa = new Ankete(naziv, opis, ime_autora, sugestija);
+	
+		return anketa;
+	}
+	
+
+	public void dohvati_anketu (Ankete anketa) {
+		
+		anketa.getNaziv();
+	}
 }
