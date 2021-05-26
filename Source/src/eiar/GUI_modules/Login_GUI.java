6770bbad8b8be76a16827036112e28de560133a2
Piero Battelli <pierobattelli@gmail.com>
2021-05-26 14:52:05 +0200

    Edited Login GUI

--- Source/src/eiar/GUI_modules/Login_GUI.java
+++ Source/src/eiar/GUI_modules/Login_GUI.java
@@ -10,6 +10,7 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
 
 public class Login_GUI extends JFrame {
 
@@ -30,12 +31,12 @@ public class Login_GUI extends JFrame {
 		contentPane.setLayout(null);
 		
 		Username = new JTextField();
-		Username.setBounds(22, 57, 86, 20);
+		Username.setBounds(170, 104, 86, 20);
 		contentPane.add(Username);
 		Username.setColumns(10);
 		
 		Password = new JTextField();
-		Password.setBounds(22, 113, 86, 20);
+		Password.setBounds(170, 157, 86, 20);
 		contentPane.add(Password);
 		Password.setColumns(10);
 		
@@ -48,7 +49,19 @@ public class Login_GUI extends JFrame {
 				
 			}
 		});
-		Submit.setBounds(22, 172, 89, 23);
+		Submit.setBounds(170, 205, 89, 23);
 		contentPane.add(Submit);
+		
+		JLabel lblNewLabel = new JLabel("Username:");
+		lblNewLabel.setBounds(93, 107, 67, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Password:");
+		lblNewLabel_1.setBounds(93, 160, 67, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("LOGIN");
+		lblNewLabel_2.setBounds(194, 46, 74, 33);
+		contentPane.add(lblNewLabel_2);
 	}
 }
