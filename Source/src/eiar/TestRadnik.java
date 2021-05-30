e94107b6f72441574bbec3f08dbc04f065673b36
vedranmatic <vmatic@riteh.hr>
2021-05-30 14:40:41 +0200

    updating test classes

--- Source/src/eiar/TestRadnik.java
+++ Source/src/eiar/TestRadnik.java
@@ -14,12 +14,12 @@ public class TestRadnik {
 		Date rvp = new Date(2,3,2004);//pocetak radnog vremena
 		Date rvk = new Date(5,7,2003);//kraj radnog vremena
 		Radnik r1 = new Radnik("Mateo","Mateov",date,"Zagreb","Zagrebacka ulica","MateoMateov@gmail.com",87654321,"0911237894","Riteh","Radnik",rvp,rvk,3);
-		assertEquals(null,r1.getId());
+		assertEquals(0,r1.getId());
 		
 		Date datef = new Date(1,2,2012);
 		Financijska_izvjesca f1 = new Financijska_izvjesca("project #1 chat room","opis","Ime Autora",1000,800,datef,"format");
 		
-		String s = "naziv: " + "project #1 chat room" + "\r\nopis: " + "opis" + "\r\nime autora: " + "Ime Autora" + "\r\nprihodi: " + "1000" + "\r\nrashodi: " + "800" + "\r\ndatum: " + datef.toString();
+		String s = "naziv: " + "project #1 chat room" + "\r\nopis: " + "opis" + "\r\nime autora: " + "Ime Autora" + "\r\nprihodi: " + "1000.0" + "\r\nrashodi: " + "800.0" + "\r\ndatum: " + datef.toString();
 		
 		assertEquals(s,r1.Dohvati_izvjesce(f1));
 		
